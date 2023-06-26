861d1bea6bb8bbeee3129964014ad8704e0932eb
EC2 Default User <ec2-user@ip-172-31-46-160.ap-northeast-1.compute.internal>
2023-06-26 05:31:24 +0000

    [now] 4-kadai

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,7 @@
 class BooksController < ApplicationController
   def new
     @book = Book.new
+    redirect_to book_path(@book)
   end
 
   def create
@@ -12,9 +13,11 @@ class BooksController < ApplicationController
 
   def index
     @book = Book.new
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :views
   devise_for :users
-  root to: 'homes#top'
+  root to: 'homes#about'
 
   resources :books, only: [:create, :index, :show]
-  resources :users, only: [:show, :edit, :index]
+  resources :users, only: [:show, :edit, :update, :index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
