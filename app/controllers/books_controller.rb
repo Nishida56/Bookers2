0fc61f33efe9bbd9604952a119f65f77354555a4
EC2 Default User <ec2-user@ip-172-31-46-160.ap-northeast-1.compute.internal>
2023-06-26 09:51:59 +0000

    [now] 4-kadai

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,7 +14,7 @@ class BooksController < ApplicationController
   def index
     @book = Book.new
     @books = Book.all
-    @user = @book.user
+    @user = User.find(current_user.id)
   end
 
   def show
@@ -23,7 +23,21 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
 
   private
 
